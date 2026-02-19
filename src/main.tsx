7658a6434168e99bfa722368141163c2a06cb1eb
acehidan <aungaungoogame2@gmail.com>
2026-02-19 15:04:17 +0630

    fix deploy err

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
-import { ThemeProvider } from "./components/theme-provider.tsx";
+import { ThemeProvider } from "./components/theme-provider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
